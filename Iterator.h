987e529c9fcff81a7f983d00b1acce07080bcea5
CamRobinson <robinsoncameron1103@gmail.com>
2021-02-03 16:53:34 -0600

    Added functions

---
+++ Iterator.h
@@ -0,0 +1,10 @@
+#pragma once
+class Iterator
+{
+public:
+
+
+private:
+	<Node>
+};
+
---
+++ Lists.h
@@ -0,0 +1,14 @@
+template<typename T>
+class Lists
+{
+public:
+
+
+
+
+private:
+	Node<T> m_first;
+	Node<T> m_last;
+	int m_nodeCount;
+};
+
